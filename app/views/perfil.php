374bbc9de8175422758ba3d19def625c025fce2e
DeyvideDhyonata <133993847+DeyvideDhyonata@users.noreply.github.com>
2024-08-16 17:25:21 -0300

    Atualização

--- app/views/perfil.php
+++ app/views/perfil.php
@@ -9,23 +9,13 @@
 <body>
     <div class="container" id="container" >
 
-        <div class="form-container sign-in">
+        <div class="containerUser">
             <h1>Perfil</h1>
-            <img src="" alt="Imagem do Usuário">
+            <img src="" class="imageUser" alt="Imagem do Usuário">
         </div>
 
-        <div class="form-container sign-up">
+        <!-- <div class="form-container sign-in">
             <form action="" class="cadForm" method="POST">
-                <h1>Registre-se</h1>
-                    <div class="social-icons">
-                        <!-- Site font awesome -->
-                        <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
-                    </div>
-
-                    <span>Crie sua conta</span>
 
                     <div class='alertcadIncorreto alert-dangercad' role='alert'>
                         <span>Usuário não foi cadastrado!!</span>
@@ -35,36 +25,18 @@
                         <span>Usuário cadastrado com sucesso!!</span>
                     </div>
 
-                    <input type="text" name="nome" class="regNome required" placeholder="Nome" oninput="registerNome()">
+                    <input type="file" name="foto" class="regFoto required" placeholder="Nome" oninput="registerNome()">
                     <span class="span-required">Preencha o nome corretamente!</span>
 
-                    <input type="text" name="sobrenome" class="regSobrenome required" placeholder="Sobrenome" oninput="registerSobrenome()">
-                    <span class="span-required">Preencha o sobrenome corretamente!</span>
-
-                    <input type="email" name="email" class="regEmail required" placeholder="E-mail" oninput="registerEmail()">
-                    <span class="span-required">Preencha o email corretamente!</span>
-
-                    <input type="password" name="senha" class="regSenha required" placeholder="Senha" oninput="registerSenha()">
-                    <span class="span-required">A senha precisa ter 6 digitos no mínimo!</span>
-
-                    <button type="submit" class="btnRegistrar">Criar</button>
+                    <button type="submit" class="btnRegistrar">Alterar</button>
             </form>
-        </div>
+        </div> -->
 
         <div class="toggle-container">
             <div class="toggle">
-                <div class="toggle-panel toggle-left">
-                    <h1>Bem vindo!</h1>
-                    <p>Utilize seu e-mail para entrar na sua conta.</p>
-
-                    <button class="hidden" id="login">Entrar</button>
-                </div>
-
                 <div class="toggle-panel toggle-right">
-                    <h1>Bem Vindo!</h1>
-                    <p>Registre-se, para poder utilizar o nosso site.</p>
-
-                    <button class="hidden" id="register">Registrar</button>
+                    <h1>Informações adicionais</h1>
+                    
                 </div>
             </div>
         </div>
